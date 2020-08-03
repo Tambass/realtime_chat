bc15bb4fc478278302c188edeac426f3154fcfce
Tambass <tambass85@gmail.com>
2020-08-03 16:51:04 +0200

    Submit message (get message text + emit message to server)

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,21 @@
+const chatForm = document.getElementById("chat-form");
+
 const socket = io();
 
-socket.on("message", message => {
+socket.on("message", (message) => {
   console.log(message);
-})
+});
+
+// Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //Get message text
+  const msg = e.target.elements.msg.value;
+
+  //Emit message to server
+  socket.emit("chatMessage", msg);
+});
 
 // const chatForm = document.getElementById("chat-form");
 // const chatMessages = document.querySelector(".chat-messages");
