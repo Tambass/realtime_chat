d3275f364be9cd146e0f46ee8f43012f32e5380d
Tambass <tambass85@gmail.com>
2020-08-03 16:37:07 +0200

    broadcast when a user connect

--- public/js/main.js
+++ public/js/main.js
@@ -1,67 +1,73 @@
-const chatForm = document.getElementById("chat-form");
-const chatMessages = document.querySelector(".chat-messages");
-const roomName = document.getElementById("room-name");
-const userList = document.getElementById("users");
-
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
-});
-
 const socket = io();
 
-// Join chatroom
-socket.emit("joinRoom", { username, room });
-
-// Get room and users
-socket.on("roomUsers", ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
-});
-
-// Message from server
-socket.on("message", (message) => {
+socket.on("message", message => {
   console.log(message);
-  outputMessage(message);
+})
 
-  // Scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
+// const chatForm = document.getElementById("chat-form");
+// const chatMessages = document.querySelector(".chat-messages");
+// const roomName = document.getElementById("room-name");
+// const userList = document.getElementById("users");
 
-// Message submit
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+// // Get username and room from URL
+// const { username, room } = Qs.parse(location.search, {
+//   ignoreQueryPrefix: true,
+// });
 
-  // Get message text
-  const msg = e.target.elements.msg.value;
+// const socket = io();
 
-  // Emit message to server
-  socket.emit("chatMessage", msg);
+// // Join chatroom
+// socket.emit("joinRoom", { username, room });
 
-  // Clear input
-  e.target.elements.msg.value = "";
-  e.target.elements.msg.focus();
-});
+// // Get room and users
+// socket.on("roomUsers", ({ room, users }) => {
+//   outputRoomName(room);
+//   outputUsers(users);
+// });
 
-// Output message to DOM
-function outputMessage(message) {
-  const div = document.createElement("div");
-  div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
-  <p class="text">
-    ${message.text}
-  </p>`;
-  document.querySelector(".chat-messages").appendChild(div);
-}
+// // Message from server
+// socket.on("message", (message) => {
+//   console.log(message);
+//   outputMessage(message);
 
-// Add room name to DOM
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
+//   // Scroll down
+//   chatMessages.scrollTop = chatMessages.scrollHeight;
+// });
 
-// Add users to DOM
-function outputUsers(users) {
-  userList.innerHTML = `
-    ${users.map((user) => `<li>${user.username}</li>`).join("")}
-  `;
-}
+// // Message submit
+// chatForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   // Get message text
+//   const msg = e.target.elements.msg.value;
+
+//   // Emit message to server
+//   socket.emit("chatMessage", msg);
+
+//   // Clear input
+//   e.target.elements.msg.value = "";
+//   e.target.elements.msg.focus();
+// });
+
+// // Output message to DOM
+// function outputMessage(message) {
+//   const div = document.createElement("div");
+//   div.classList.add("message");
+//   div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+//   <p class="text">
+//     ${message.text}
+//   </p>`;
+//   document.querySelector(".chat-messages").appendChild(div);
+// }
+
+// // Add room name to DOM
+// function outputRoomName(room) {
+//   roomName.innerText = room;
+// }
+
+// // Add users to DOM
+// function outputUsers(users) {
+//   userList.innerHTML = `
+//     ${users.map((user) => `<li>${user.username}</li>`).join("")}
+//   `;
+// }
--- server.js
+++ server.js
@@ -10,6 +10,18 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+//Run when client connects
+io.on("connection", (socket) => {
+  console.log("New WS connection...");
+
+  socket.emit("message", "Welcome to LiveChat!");
+
+  // Broadcast when a user connect
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  
+});
+
 const PORT = 4000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Le server tourne sur le port ${PORT}`));
