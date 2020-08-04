16d2c4889b26ac8f81ad3f4d54d96f550aae6038
Tambass <tambass85@gmail.com>
2020-08-04 12:30:55 +0200

    add formatMessage

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,17 +11,22 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatCore Bot";
+
 //Run when client connects
 io.on("connection", (socket) => {
   //Welcome current user
-  socket.emit("message", "Welcome to LiveChat!");
+  socket.emit("message", formatMessage(botName, "Welcome to LiveChat!"));
 
   // Broadcast when a user connect
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   // Runs when client disconnect
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   //Listen for chatMessage
--- utils/messages.js
+++ utils/messages.js
@@ -1,1 +1,11 @@
-function formatMessage();
+const moment = require("moment");
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a"),
+  };
+}
+
+module.exports = formatMessage;
